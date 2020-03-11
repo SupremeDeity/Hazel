2e7de9a3ec491b5f2278f6cb64b531fb0e6bf302
Mohsin Sheikh <coinbasemohsin@gmail.com>
2020-03-12 03:04:07 +0500

    Added Mouse And Key codes

--- Hazel/src/Hazel.h
+++ Hazel/src/Hazel.h
@@ -7,6 +7,10 @@
 #include "Hazel/LayerStack.h"
 #include "Hazel/ImGui/ImGuiLayer.h"
 
+#include "Hazel/Input.h"
+#include "Hazel/KeyCodes.h"
+#include "Hazel/MouseButtonCodes.h"
+
 // ---Entry Point---------------------
 #include "Hazel/EntryPoint.h"
 // -----------------------------------
